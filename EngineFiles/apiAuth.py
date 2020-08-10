29f6d4befad0154f9134164549b85267c76bb21a
Dhika <diovinr17@gmail.com>
2020-08-10 21:54:06 +0700

    Update apiAuth.py

--- EngineFiles/apiAuth.py
+++ EngineFiles/apiAuth.py
@@ -1,6 +1,6 @@
 class apiAuth():
     def __init__(self):
-        self.access_token = "62419840-kJDnw4KGAL2HxRjRzflVV7ASVTQIRcy4Gs1vl2b2L"
-        self.access_token_secret = "sPhAPSbkQ9F09I98gQQpv17No1EcNIcWg85PcclTu7ZCE"
-        self.consumer_key = "YLhNm1LF7LlIi5m3EUGBz0FoR"
-        self.consumer_secret = "q2E9zVACctDdVCxKY0NZU14ejbNm6XWPTW9Oskw2ERFellTnjF"
+        self.access_token = "access_token"
+        self.access_token_secret = "access_token_secret"
+        self.consumer_key = "consumer_key"
+        self.consumer_secret = "consumer_secret"
